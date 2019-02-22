8d32ae91f0be5e877522956fe222d42742182051
ndambu <mercyndambu9@gmail.com>
2019-02-22 20:08:04 +0300

    changes made

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,8 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       event.preventDefault();
+$(document).ready(function() {
+  $("button").click(function(){
+    var firstQuestion = parseInt($("input:radio[name=answer]:checked")).val();
+    var secondQuestion = parseInt($("input:radio[name=answer1]:checked")).val();
+    var thirdQuestion = parseInt($("input:radio[name=answer2]:checked")).val();
+    var fourthQuestion = parseInt($("input:radio[name=answer3]:checked")).val();
+    var fifthQuestion = parseInt($("input:radio[name=answer4]:checked")).val();
+  })
